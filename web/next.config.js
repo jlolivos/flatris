82a260e5ed6b618a6b60d6f286ed405b51eb191b
jlolivos <jlolivos@gmail.com>
2026-01-13 17:05:43 -0600

    Refactor next.config.js for ESLint and Babel
    Updated webpack configuration to include ESLint settings and modified rules for Babel loader.

--- web/next.config.js
+++ web/next.config.js
@@ -1,35 +1,20 @@
-const { join, resolve } = require('path');
+const path = require('path');
 
 module.exports = {
-  webpack(config, { dev, isServer }) {
-    // Transpile shared code as well
-    config.module.rules
-      .filter(rule => rule.use && rule.use.loader === 'next-babel-loader')
-      .forEach(rule => {
-        rule.include.push(join(__dirname, '../shared'));
-        rule.use.options.configFile = resolve('./babel.config.js');
-      });
-
-    // Enable source maps in production
-    if (!dev) {
-      config.devtool = 'source-map';
-
-      if (!isServer) {
-        config.plugins.map(p => {
-          if (p.constructor.name === 'UglifyJsPlugin') {
-            p.options.sourceMap = true;
-          }
-
-          return p;
-        });
+  eslint: {
+    ignoreDuringBuilds: true // optional, skip lint errors during build
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, '../shared')],
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel', '@babel/preset-flow']
+        }
       }
-    }
-
-    // XXX: Uncomment to generate unminified production code
-    // config.plugins = config.plugins.filter(
-    //   plugin => plugin.constructor.name !== 'UglifyJsPlugin'
-    // );
-
+    });
     return config;
   }
 };
